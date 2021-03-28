ccc23bc63c49f6885678531d7e7f5787aa88b819
ndriabecks <aandreani1998@gmail.com>
2021-03-28 11:05:46 +0200

    Hello module created

---
+++ hello-module/hello.c
@@ -0,0 +1,40 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+
+#define MODULE_LOG "[+] hello: "
+#define VALUE_PERM S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Andrea Andreani <aandreani1998@gmail.com");
+MODULE_DESCRIPTION("My first attempt to code a kernel module");
+MODULE_VERSION("0.0");
+
+// This allows us to use parameters in the module
+int value = -1;
+module_param(value, int, VALUE_PERM);
+MODULE_PARM_DESC(value, "Just a value");
+
+// module_init will call this function
+static int __init dummy_init(void)
+{
+    printk(KERN_INFO "Hello, AAmodule correctly loaded\n");
+
+    printk(KERN_INFO MODULE_LOG "this is a correct print\n");
+    printk(KERN_WARNING MODULE_LOG "value = %d\n", value);
+    printk(KERN_WARNING MODULE_LOG "pid = %d\n", current->pid);
+
+    return 0;
+}
+
+// module_exit() will call this function
+// OSS: this functions require you to put a return if
+// not void
+static void __exit dummy_exit(void)
+{
+    printk(KERN_WARNING MODULE_LOG "hello is being unloaded\n");
+}
+
+module_init(dummy_init);
+module_exit(dummy_exit);
